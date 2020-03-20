e4eba15e84ab3cc7cf440b1de5ed94ceb0995173
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-20 20:59:02 +0000

    disqus

--- src/js/component/TopicTable.js
+++ src/js/component/TopicTable.js
@@ -1,29 +1,42 @@
 import React from "react";
 import "../../styles/TopicTable.scss";
+import Article from "./disqus";
 
 export function TopicTable() {
 	return (
-		<table className="table">
-			<tbody>
-				<tr>
-					<th scope="row">Icon</th>
-					<td>PlayStation</td>
-					<td>threads/replies</td>
-					<td>Time last posted</td>
-				</tr>
-				<tr>
-					<th scope="row">Icon</th>
-					<td>Xbox</td>
-					<td>threads/replies</td>
-					<td>Time last posted</td>
-				</tr>
-				<tr>
-					<th scope="row">Icon</th>
-					<td>Nintendo</td>
-					<td>threads/replies</td>
-					<td>Time last posted</td>
-				</tr>
-			</tbody>
-		</table>
+		<div className="container">
+			<h2 className="text-center">Top Games Today</h2>
+			<div className="card-deck">
+				<div className="row">
+					<div className="card gameCard">
+						<img src="https://via.placeholder.com/150x300" className="card-img" alt="..." />
+						<div className="card-img-overlay">
+							<h5 className="card-title">League Of Legends</h5>
+						</div>
+					</div>
+					<div className="card gameCard">
+						<img src="https://via.placeholder.com/150x300" className="card-img" alt="..." />
+						<div className="card-img-overlay">
+							<h5 className="card-title">Team Fortress 2</h5>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="card gameCard">
+						<img src="https://via.placeholder.com/150x300" className="card-img" alt="..." />
+						<div className="card-img-overlay">
+							<h5 className="card-title">Sonic</h5>
+						</div>
+					</div>
+					<div className="card gameCard">
+						<img src="https://via.placeholder.com/150x300" className="card-img" alt="..." />
+						<div className="card-img-overlay">
+							<h5 className="card-title">Grand Theft Auto</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Article />
+		</div>
 	);
 }
---
+++ src/js/component/disqus.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Disqus from "disqus-react";
+import "../../styles/Disqus.scss";
+
+export default class Article extends Component {
+	render() {
+		const disqusShortname = "finalproject-3"; //found in your Disqus.com dashboard
+		const disqusConfig = {
+			url: "https://3000-f35affae-5763-4ad0-b985-2408c104379b.ws-us02.gitpod.io/", //this.props.pageUrl
+			identifier: "article-id", //this.props.uniqueId
+			title: "Title of Your Article" //this.props.title
+		};
+
+		return (
+			<div className="article-container">
+				<h1>Article Title</h1>
+
+				<p>Article content.</p>
+
+				<Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+			</div>
+		);
+	}
+}
