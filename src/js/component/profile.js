5695a06db4854ff4f39a22c247fa994755a0c603
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-24 20:12:24 +0000

    hover

--- src/js/component/profile.js
+++ src/js/component/profile.js
@@ -30,55 +30,45 @@ export function Profile() {
 			<div className="container profileContainer">
 				<h2 className="text-center">Favorite Games</h2>
 				<div className="card-deck profileCardDeck">
-					<div className="card gameCard">
+					<div className="card gameCard hover">
 						<img
 							src="https://static-cdn.jtvnw.net/ttv-boxart/League%20of%20Legends.jpg"
-							className="card-img"
+							className="card-img gameCard"
 							alt="..."
 						/>
-						<div className="card-img-overlay">
-							<h5 className="card-title">League Of Legends</h5>
-						</div>
+						<div className="card-img-overlay" />
 					</div>
-					<div className="card gameCard">
+					<div className="card gameCard hover">
 						<img
 							src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Tf2_standalonebox.jpg/220px-Tf2_standalonebox.jpg"
-							className="card-img"
+							className="card-img gameCard"
 							alt="..."
 						/>
-						<div className="card-img-overlay">
-							<h5 className="card-title">Team Fortress 2</h5>
-						</div>
+						<div className="card-img-overlay" />
 					</div>
-					<div className="card gameCard">
+					<div className="card gameCard hover">
 						<img
 							src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/game-info/ac1_nakedbox_mobile_164648.jpg"
-							className="card-img"
+							className="card-img gameCard"
 							alt="..."
 						/>
-						<div className="card-img-overlay">
-							<h5 className="card-title">Sonic</h5>
-						</div>
+						<div className="card-img-overlay" />
 					</div>
-					<div className="card gameCard">
+					<div className="card gameCard hover">
 						<img
 							src="https://store-images.s-microsoft.com/image/apps.13587.70644597693904836.cde9dd08-509f-4054-8190-24728e59ad5e.b122adbd-e812-41be-bc09-4dd7b0078036?mode=scale&q=90&h=300&w=200"
-							className="card-img"
+							className="card-img gameCard"
 							alt="..."
 						/>
-						<div className="card-img-overlay">
-							<h5 className="card-title">Grand Theft Auto</h5>
-						</div>
+						<div className="card-img-overlay" />
 					</div>
-					<div className="card gameCard">
+					<div className="card gameCard hover">
 						<img
 							src="https://www.residentevil.com/_asset/images/re2-panel.jpg"
-							className="card-img"
+							className="card-img gameCard"
 							alt="..."
 						/>
-						<div className="card-img-overlay">
-							<h5 className="card-title">Resident Evil</h5>
-						</div>
+						<div className="card-img-overlay" />
 					</div>
 				</div>
 			</div>
