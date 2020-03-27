f57f79948045abaf1bc2cc0a4e7ed52622a79860
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-03-27 19:18:41 +0000

    fetch with card

--- src/js/component/TopicTable.js
+++ src/js/component/TopicTable.js
@@ -11,19 +11,20 @@ export function TopicTable() {
 	return (
 		<div className="container tableContainer">
 			<h2 className="text-center">Games Of The Day</h2>
-			<div className="card-deck homeCardDeck justify-content-center">
+			<div className="row row-cols-1 row-cols-md-4">
 				{!store.games
 					? "Loading..."
 					: store.games.map((t, index) => (
-							<div className="card gameCard" key={index}>
-								<img src="/" className="card-img gameCard" alt="..." />
-								<div className="card-img-overlay">
-									<h5 className="card-title">{t.name}</h5>
-									<p className="card-text text-right">
-										<button type="button" className="btn btn-success favbtn ripple constantHover">
-											<i className="fas fa-plus" />
-										</button>
-									</p>
+							<div className="col mb-4" key={index}>
+								<div className="card">
+									<img src={t.background_image} className="card-img-top" alt="..." />
+									<div className="card-body">
+										<h5 className="card-title">{t.name}</h5>
+										<p className="card-text">
+											This is a longer card with supporting text below as a natural lead-in to
+											additional content. This content is a little bit longer.
+										</p>
+									</div>
 								</div>
 							</div>
 					  ))}
